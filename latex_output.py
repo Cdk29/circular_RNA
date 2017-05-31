c66a38294889590f5f83630091b51f7201914880
Etienne Rolland <etienne.rolland.cdk@gmail.com>
2017-05-31 12:09:02 +0200

    Latex output function for mfe moved into the script for latex output

--- latex_output.py
+++ latex_output.py
@@ -1,9 +1,37 @@
-import re
-import os
 import requests
 
 
+
+
+
+def RNAfold_latex_output(best_circ, best_score, best_seq):
     
+    name_best_cir=str(best_circ)
+    name_best_cir=name_best_cir.replace("_", "\_")
+    name_best_cir=name_best_cir.replace(">", "$>$")
+    best_seq=best_seq.replace("\n", "")
+    
+    string=""
+    string+="\\section{Calculation of the minimum free energy of the circular RNAs}"+ "\n" 
+    string+="In this section is report the calculation of the minimum free energy of all the circular RNAs outputed by the design script" +  "\\newline "
+    string+="The calculation have been made using RNAfold of the package ViennaRNA 2.3.5." "\\newline "
+    string+="Among all the circular RNAs present in the file named \"circular\_design\_output\", the one with the lower minimum free energy is " + name_best_cir + "\\newline "
+    string+="The minimum free energy for this design is " + str(best_score) + "kcal/mol" +"\\newline "
+    string+="Which sequence is :" + "\\newline " + "\\newline "+ "5'  " + "\\seqsplit{%" + "\n"
+    string+= str(best_seq) + "}" + "  3'" + "\n" 
+
+    tex_file = "design_report.tex"
+    
+    
+    fichier=open(tex_file, "a")
+    fichier.write(string)
+    fichier.close()
+    
+    return
+
+
+
+
 
 def latex_output_miranda(list_of_dictionnary):
     
